308f9aa992ac337cc0d7589bffd8adc9626593c7
JH-BAIK <dorianbjh@gmail.com>
2025-05-14 17:44:17 +0900

    update DB column (category, subcategory, synonym, antonym) webpage update

--- app/models.py
+++ app/models.py
@@ -8,9 +8,13 @@ class Word(Base):
     __tablename__ = 'words'
 
     id = Column(Integer, primary_key=True)
+    category = Column(String)
+    subcategory = Column(String)
     english = Column(String)
     pronunciation = Column(String)
     part_of_speech = Column(String)
     meaning = Column(String)
+    synonym = Column(String)
+    antonym = Column(String)
     example_sentence = Column(String)
     example_translation = Column(String)
