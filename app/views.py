fdc9228b4283a322f60321d11603184f7d3d4e11
JH-BAIK <dorianbjh@gmail.com>
2025-05-14 16:34:23 +0900

    delete init_db.py add column (category, subcategory, synonym, antonym)

--- app/views.py
+++ app/views.py
@@ -16,22 +16,12 @@ def home():
 
 @main.route('/search')
 def search():
-    from sqlalchemy import func
     query = request.args.get('q', '').strip()
-    # sort = request.args.get('sort', 'english')
     order = request.args.get('order', 'asc')
     results = []
 
     if query:
         q = session.query(Word).filter(Word.english.ilike(f'%{query}%'))
-
-        # if sort == 'meaning':
-        #     col = Word.meaning
-        # elif sort == 'example_len':
-        #     col = func.char_length(Word.example_sentence)
-        # else:
-        #     col = Word.english
-
         sort_column = Word.english
 
         if order == 'desc':
@@ -40,9 +30,5 @@ def search():
             q = q.order_by(sort_column.asc())
 
         results = q.all()
-
-        # results = (session.query(Word)
-        #     .filter(Word.english.ilike(f'%{query}%'))
-        #     .all())
 
     return render_template('search.html', query=query, results=results)
--- init_db.py
+++ init_db.py
@@ -1,23 +1,29 @@
 # init_db.py
 import pandas as pd
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, synonym
 from app.models import Base, Word
 
 DATABASE_URL = 'sqlite:///wordbook.db'
-EXCEL_PATH = 'data/00_수단비 통합_0326_수정(0507)_발음기호수정_B.xlsx'
+EXCEL_PATH = 'data/00_수단비 통합_위의파일컬럼_색출중.xlsx'
 
 def load_excel_to_db():
     df = pd.read_excel(EXCEL_PATH)
 
     # 필요한 컬럼만 추출 및 이름 매핑
     df = df[[
-        '영어', '발음기호', '품사', '단어 뜻', 'YK 예문', 'YK 번역'
+        '카테고리', '카테고리_보조',
+        '영어', '발음기호', '품사', '단어 편집_최종',
+        '유의어_최종', '반의어_최종', 'YK 예문', 'YK 번역'
     ]].rename(columns={
+        '카테고리': 'category',
+        '카테고리_보조': 'subcategory',
         '영어': 'english',
         '발음기호': 'pronunciation',
         '품사': 'part_of_speech',
-        '단어 뜻': 'meaning',
+        '단어 편집_최종': 'meaning',
+        '유의어_최종': 'synonym',
+        '반의어_최종': 'antonym',
         'YK 예문': 'example_sentence',
         'YK 번역': 'example_translation',
     })
@@ -31,10 +37,14 @@ def load_excel_to_db():
 
     for _, row in df.iterrows():
         word = Word(
+            category = row['category'],
+            subcategory = row['subcategory'],
             english=row['english'],
             pronunciation=row['pronunciation'],
             part_of_speech=row['part_of_speech'],
             meaning=row['meaning'],
+            synonym=row['synonym'],
+            antonym=row['antonym'],
             example_sentence=row['example_sentence'],
             example_translation=row['example_translation'],
         )
